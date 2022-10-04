750ad7efd644f676998b7dbf7f85a84860f4fb6a
victor <dvicxy@gmail.com>
2022-10-04 14:27:53 +0100

    splash screen initialised

---
+++ gopages/lib/pages/assesory/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+var header = const TextStyle(
+    color: Colors.black, fontSize: 25.0, fontWeight: FontWeight.bold);
--- gopages/lib/pages/splash_page.dart
+++ gopages/lib/pages/splash_page.dart
@@ -11,7 +11,15 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column()),
+      body: SafeArea(
+          child: Column(
+        children: [
+          const Text(
+            "Welcome to the Pages login to proceed",
+            style: TextStyle(color: Colors.black),
+          )
+        ],
+      )),
     );
   }
 }
